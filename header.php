ccd08683fce7174b1b1b988a41d8ce4cab439b91
42143 <aslan-k@hotmail.com>
2017-05-16 14:46:43 -0300

    header

---
+++ header.php
@@ -0,0 +1,87 @@
+<header>
+    <div class="container-fluid main">
+        <div class="row">
+            <div class="col-md content">
+                <div class="nivelk">NIVELK</div>
+                <div class="menu_bar">
+                    <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+                </div>
+                    <nav>
+                        <ul>
+                            <li><a href="http://nivelk.com.br/index.php">Home</a></li>
+                            <li>Serviços 
+                                <i class="fa fa-angle-double-down fa-lg"></i>
+                                <i class="fa fa-angle-double-up fa-lg"></i>
+                            </li>
+                            <li><a href="http://nivelk.com.br/portfolio.php">Portfólio</a></li>
+                            <li><a href="http://nivelk.com.br/client.php">Clientes</a></li>
+                            <li><a href="http://nivelk.com.br/agency.php">Agência</a></li>
+                            <li><a href="http://nivelk.com.br/blog.php">Blog</a></li>
+                            <li><a class="blue" href="http://nivelk.com.br/login.php">Faça login</a></li>
+                            <li><a class="blue" href="http://nivelk.com.br/register.php">Cadastre-se</a></li>
+                           <button>ORÇAMENTO</button>
+                        </ul>
+                    </nav>
+            </div>    
+        </div>
+        <div class="menu_trop">
+            <div class="row">
+                <div class="col">
+                        <ul>
+                                <li>IDENTIDADE VISUAL</li>
+                                <li></li>
+                                <li>Criação de Logotipo</li>
+                                <li>Identidade Visual</li>
+                                <li>Impressão</li>
+                        </ul>
+                    </div>
+                    <div class="col">
+						<ul>
+							<li>CRIAÇÂO DE SITE</li>
+							<li></li>
+							<li>Criação de Site</li>
+							<li>Loja Virtual</li>
+							<li>Landing Page</li>
+						</ul>
+                     </div>    
+                    <div class="col">    
+						<ul>
+							<li>MARKETING DIGITAL</li>
+							<li></li>
+							<li>MDP</li>
+							<li>Otimização de sites SEO</li>
+							<li>Links Patrocinados</li>
+							<li>Mídias Sociais</li>
+							<li>E-mail Marketing</li>
+						</ul>
+                     </div>    
+                    <div class="col">    
+						<ul>
+							<li>SUPORTE</li>
+							<li></li>
+							<li>Gentão de Sites</li>
+						</ul>
+                     </div> 
+                    <div class="col">    
+						<ul>
+							<li>SISTEMAS DE GENTÂO</li>
+							<li></li>
+							<li>Sistema de Advocacia</li>
+							<li>Sistema de odontológico</li>
+							<li>Sistema para loja</li>
+						</ul>
+                     </div>    
+                    <div class="col">    
+						<ul>
+							<li>BRIEFING</li>
+							<li></li>
+							<li>Briefing Logotipo</li>
+							<li>Briefing Site</li>
+							<li>Briefing Link Patrocinado</li>
+							<li>Briefing Loja Virtual</li>
+						</ul>
+                     </div>    
+            </div>
+        </div>
+    </div>
+</header>
