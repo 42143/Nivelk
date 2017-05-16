980e9b9a3b173d94e91dd6af6cc9d8792d8f6a1c
42143 <aslan-k@hotmail.com>
2017-05-16 14:48:38 -0300

    INDEX

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+	<head>
+		<?php require_once"head.php";?>
+		<!--SASS-->
+		<link rel="stylesheet" href="sass/pages/index.css"/>
+		<link rel="stylesheet" href="sass/layout/header.css"/>
+        <link rel="stylesheet" href="sass/componentes/font-awesome-4.7.0/css/font-awesome.min.css"/>
+		<title>Agência Digital | Nivelk</title>
+	</head>
+	<body>
+        <div class="main">
+            <div class="container center">
+                
+                <div class="row">
+                    <div class="col-sm-3">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-3">
+
+                    </div>
+                </div>
+            </div>    
+        </div>
+		<?php require_once"header.php";?>
+	</body>
+</html>
