dbb49618df470af37d4ed94e50b392bf9fde7e8d
42143 <aslan-k@hotmail.com>
2017-05-18 04:24:39 -0300

    INDEX

--- index.php
+++ index.php
@@ -4,31 +4,149 @@
 		<?php require_once"head.php";?>
 		<!--SASS-->
 		<link rel="stylesheet" href="sass/pages/index.css"/>
-		<link rel="stylesheet" href="sass/layout/header.css"/>
         <link rel="stylesheet" href="sass/componentes/font-awesome-4.7.0/css/font-awesome.min.css"/>
 		<title>Agência Digital | Nivelk</title>
 	</head>
 	<body>
         <div class="main">
-            <div class="container center">
-                
-                <div class="row">
-                    <div class="col-sm-3">
-
+            <div class="Screen screen1">   
+                <div class="container">
+                    <div class="row">
+                        <div class="col"><h1>RENOVE SUAS IDEIAS,<br>QUE A GENTE JUNDA.</h1></div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <img class="img" src="img/icon1.png" alt="icone logotipo"/>
+                            <h3>Logotipo</h3>
+                            <p>Seu logotipo é o ponto de partida para causar uma boa impressão do seu negócio.</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="img/icon2.png" alt="icone identidade virtual"/>
+                            <h3>Identidade Visual</h3>
+                            <p>Todo seu material de trabalho precisa transmitir sua imagem e o propósito do seu negócio.</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="img/icon3.png" alt="icone site responsivo"/>
+                            <h3>Site Responsivo</h3>
+                            <p>Transmita mais credibilidade com um site responsivo e design moderno e profissional.</p>
+                        </div>
+                        <div class="col">
+                            <img class="img" src="img/icon4.png" alt="icone marketing digital"/>
+                            <h3>Marketing digital</h3>
+                            <p>SEO é fundamental na estratégia do seu negócio, mais visibilidade, mais oportunidades.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-3">
-
+            </div>
+            <div class="Screen screen2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <h1>Não perca mais tempo, em ter um upgrade no seu negócio e melhore o empreendimento.</h1>
+                            <p>Os nosso Serviços vão melhorar o desempenho da sua empresa ou do seu negócio, ajustando à forma de empreender,lhe dando mais visão no ramo de atuação, buscando o público alvo com google analytics, adwords, alavancando e direcionado todas as estratégia de marketing e proporcionado um design bem trabalhado em seu site.</p> 
+							<p>Não esqueçar de ter newsletter da Nivelk no seu e-mail, conteúdo de MDP, sites, identidade visual e perguntas relacionado ao seu negócio estamos aqui para ajudar.</p>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="col">
+                                    <h3>Nossos Serviços</h3>
+                                </div>
+                                <div class="w-100"></div>
+                                <div class="col">
+                                    <div class="tituloIcone">
+                                        <div class="circulo">
+                                            <img src="img/icon5.png" alt="icone logotipo"/>
+                                        </div>
+                                        <p>Logotipo</p>
+                                    </div>
+                                    <div class="tituloIcone">
+                                        <div class="circulo">    
+                                            <img src="img/icon6.png" alt="icone criação de sites"/>
+                                        </div> 
+                                        <p>Criação de Site</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">
+                                            <img src="img/icon7.png" alt="icone MDP"/>
+                                        </div>
+                                        <p>MDP</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon8.png" alt="icone midias sociais"/>
+                                        </div>
+                                        <p>Mídias Sociais</p>    
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon9.png" alt="icone identidade visual"/>
+                                        </div>
+                                        <p>Identidade Visual</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon10.png" alt="icone lojas virtuais"/>
+                                        </div>
+                                        <p>Loja Virtual</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon11.png" alt="icone seo otimização"/>
+                                        </div>
+                                        <p>SEO Otimização</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon12.png" alt="icone gentão de sites"/>
+                                        </div>
+                                        <p>Gentão de Sites</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon13.png" alt="icone impressão"/>
+                                        </div>
+                                        <p>Impressão</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon14.png" alt="icone landing page"/>
+                                        </div>
+                                        <p>Landing Pages</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon15.png" alt="icone link patrocinado"/>
+                                        </div>
+                                        <p>Link Patrocinado</p>
+                                    </div>
+                                    <div class="tituloIcone">    
+                                        <div class="circulo">    
+                                            <img src="img/icon16.png" alt="icone sistemas de gentão"/>
+                                        </div>
+                                        <p>Sistemas de Gentão</p>
+                                    </div>    
+                                </div>    
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <center>
+                                <button class="btn btn-outline-primary btnServicos">Entenda sobre os Serviços</button>
+                            </center>    
+                        </div>
+                        <div class="col newsletter">
+                            <form class="form-group">
+                                <label for="InputEmail1">
+                                    <i class="fa fa-list-alt fa-lg" aria-hidden="true"></i>
+                                    Receba nosso Newsletter
+                                </label>
+                                <input type="email" class="form-control" id="InputEmail1" aria-describedby="emailHelp" placeholder="Digite seu email"/>
+                                <button class="btn btn-outline-primary btn-block">Enviar</button>
+                            </form>    
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-3">
-
-                    </div>
-                </div>
-            </div>    
-        </div>
+            </div>
+        </div>    
 		<?php require_once"header.php";?>
 	</body>
 </html>
