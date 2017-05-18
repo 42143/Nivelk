e533e1b49d4b365e6dde9b571e5bce6be35809cc
42143 <aslan-k@hotmail.com>
2017-05-18 04:23:46 -0300

    header

--- header.php
+++ header.php
@@ -2,9 +2,10 @@
     <div class="container-fluid main">
         <div class="row">
             <div class="col-md content">
-                <div class="nivelk">NIVELK</div>
+                <a href="http://nivelk.com.br/index.php"><div class="nivelk">NIVELK</div></a>
                 <div class="menu_bar">
                     <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
+                    <i class="fa fa-window-close-o fa-2x" aria-hidden="true"></i>
                 </div>
                     <nav>
                         <ul>
@@ -14,7 +15,6 @@
                                 <i class="fa fa-angle-double-up fa-lg"></i>
                             </li>
                             <li><a href="http://nivelk.com.br/portfolio.php">Portfólio</a></li>
-                            <li><a href="http://nivelk.com.br/client.php">Clientes</a></li>
                             <li><a href="http://nivelk.com.br/agency.php">Agência</a></li>
                             <li><a href="http://nivelk.com.br/blog.php">Blog</a></li>
                             <li><a class="blue" href="http://nivelk.com.br/login.php">Faça login</a></li>
@@ -26,15 +26,37 @@
         </div>
         <div class="menu_trop">
             <div class="row">
+                <div class="col menuMobile">
+                    <ul>
+                        <li>MENU</li>
+                        <li></li>
+                        <li>Portfólio</li>
+                        <li>Agência</li>
+                        <li>Blog</li>
+                        <li>Faça login</li>
+                        <li>Cadastre-se</li>
+                    </ul>
+                </div>
+                <div class="col menuMobile">
+                    <ul>
+                        <li>SERVIÇOS</li>
+                        <l1></l1>
+                        <li>Identidade Visual</li>
+                        <li>Criação de Site</li>
+                        <li>Marketing Digital</li>
+                        <li>Suporte</li>
+                        <li>Sistemas de Gentão</li>
+                    </ul>
+                </div>
                 <div class="col">
-                        <ul>
-                                <li>IDENTIDADE VISUAL</li>
-                                <li></li>
-                                <li>Criação de Logotipo</li>
-                                <li>Identidade Visual</li>
-                                <li>Impressão</li>
-                        </ul>
-                    </div>
+                    <ul>
+                        <li>IDENTIDADE VISUAL</li>
+                        <li></li>
+                        <li>Criação de Logotipo</li>
+                        <li>Identidade Visual</li>
+                        <li>Impressão</li>
+                    </ul>
+                </div>
                     <div class="col">
 						<ul>
 							<li>CRIAÇÂO DE SITE</li>
@@ -71,7 +93,7 @@
 							<li>Sistema para loja</li>
 						</ul>
                      </div>    
-                    <div class="col">    
+                    <div class="col menuMobile">    
 						<ul>
 							<li>BRIEFING</li>
 							<li></li>
