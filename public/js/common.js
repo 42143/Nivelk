abd2011ec16820a3311bca78d34840b95b27accb
42143 <aslan-k@hotmail.com>
2017-07-12 17:24:56 -0300

    atualização do sistema

---
+++ public/js/common.js
@@ -0,0 +1,38 @@
+$(function(){
+	$("li:nth-child(2)").click(function(){
+        $(".servico").toggle();
+        $(".menu_trop").toggle();
+    });
+    
+    $(".menu_bar").click(function(){
+        $(".menubar").toggle();
+        $(".menu_trop").toggle();
+    });
+        //
+        var HeightWindow = $(window).height();
+        var footer = $(".footer").height();
+        var top = (HeightWindow - footer);
+    
+        $(".Screen").css({"height": HeightWindow+"px"});
+        $(".top").css({"height":top+"px"});
+    
+    setInterval(function(){
+        var HeightWindow = $(window).height();
+        var footer = $(".footer").height();
+        var top = (HeightWindow - footer); 
+        
+        if(window.matchMedia("(max-width: 991px)").matches){
+            
+            $(".Screen").css({"height":"auto"});
+            $(".top").css({"height":"auto"});
+            
+        }else{
+            
+            $(".Screen").css({"height": HeightWindow+"px"});
+            $(".top").css({"height":top+"px"});
+            
+        }
+    },500);
+    
+    //
+});
---
+++ resources/views/index.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="br" >
+	<head>
+		<?php //require_once"head.php"; ?>
+        <!--ScrollReveal-->
+        <script src="js/componentes/scrollreveal/scrollreveal.min.js"></script>
+        <script>
+            var config = {reset:true};
+            window.sr = ScrollReveal(config);
+        </script>
+        <style>
+            .sr .hero{ visibility: hidden;}
+            .sr .intro{ visibility: hidden;}
+            .sr .block{ visibility: hidden;}
+        </style>
+		<!--SASS-->
+		<link rel="stylesheet" href="css/pages/index.css"/>
+        <link rel="stylesheet" href="css/componentes/font-awesome-4.7.0/css/font-awesome.min.css"/>
+		<title>Agência Digital | Nivelk</title>
+	</head>
+	<body>
+        <div class="main">
+            <div class="Screen screen1">   
+                <div class="container">
+                    <div class="row">
+                        <div class="col hero"><h1>RENOVE SUAS IDEIAS,<br>QUE A GENTE AJUDA.</h1></div>
+                        <div class="w-100"></div>
+                        <div class="col hero">
+                            <img class="img" src="img/icon1.png" alt="icone logotipo"/>
+                            <h3>Logotipo</h3>
+                            <p>Seu logotipo é o ponto de partida para causar uma boa impressão do seu negócio.</p>
+                        </div>
+                        <div class="col hero">
+                            <img class="img" src="img/icon2.png" alt="icone identidade virtual"/>
+                            <h3>Identidade Visual</h3>
+                            <p>Todo seu material de trabalho precisa transmitir sua imagem e o propósito do seu negócio.</p>
+                        </div>
+                        <div class="col hero">
+                            <img class="img" src="img/icon3.png" alt="icone site responsivo"/>
+                            <h3>Site Responsivo</h3>
+                            <p>Transmita mais credibilidade com um site responsivo e design moderno e profissional.</p>
+                        </div>
+                        <div class="col hero">
+                            <img class="img" src="img/icon4.png" alt="icone marketing digital"/>
+                            <h3>Marketing digital</h3>
+                            <p>SEO é fundamental na estratégia do seu negócio, mais visibilidade, mais oportunidades.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="Screen screen2">
+                <div class="container">
+                    <div class="row">
+                        <div class="col block">
+                            <h1>Não perca mais tempo, em ter um upgrade no seu negócio e melhore o empreendimento.</h1>
+                            <p>Os nosso Serviços vão melhorar o desempenho da sua empresa ou do seu negócio, ajustando à forma de empreender,lhe dando mais visão no ramo de atuação, buscando o público alvo com google analytics, adwords, alavancando e direcionado todas as estratégia de marketing e proporcionado um design bem trabalhado em seu site.</p>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="col block">
+                                    <h3>Nossos Serviços</h3>
+                                </div>
+                                <div class="w-100"></div>
+                                <div class="col">
+                                    <div class="tituloIcone intro">
+                                        <div class="circulo">
+                                            <img src="img/icon5.png" alt="icone logotipo"/>
+                                        </div>
+                                        <p>Logotipo</p>
+                                    </div>
+                                    <div class="tituloIcone intro">
+                                        <div class="circulo">    
+                                            <img src="img/icon6.png" alt="icone criação de sites"/>
+                                        </div> 
+                                        <p>Criação de Site</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">
+                                            <img src="img/icon7.png" alt="icone MDP"/>
+                                        </div>
+                                        <p>MDP</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon8.png" alt="icone midias sociais"/>
+                                        </div>
+                                        <p>Mídias Sociais</p>    
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon9.png" alt="icone identidade visual"/>
+                                        </div>
+                                        <p>Identidade Visual</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon10.png" alt="icone lojas virtuais"/>
+                                        </div>
+                                        <p>Loja Virtual</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon11.png" alt="icone seo otimização"/>
+                                        </div>
+                                        <p>SEO Otimização</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon12.png" alt="icone gentão de sites"/>
+                                        </div>
+                                        <p>Gentão de Sites</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon13.png" alt="icone impressão"/>
+                                        </div>
+                                        <p>Impressão</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon14.png" alt="icone landing page"/>
+                                        </div>
+                                        <p>Landing Pages</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon15.png" alt="icone link patrocinado"/>
+                                        </div>
+                                        <p>Link Patrocinado</p>
+                                    </div>
+                                    <div class="tituloIcone intro">    
+                                        <div class="circulo">    
+                                            <img src="img/icon16.png" alt="icone sistemas de gentão"/>
+                                        </div>
+                                        <p>Sistemas de Gentão</p>
+                                    </div>    
+                                </div>    
+                            </div>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <button class="btn btn-outline-primary block">Entenda sobre os Serviços</button>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="Screen screen3">
+                <div class="container">
+                    <div class="row">
+                        <div class="col block">
+                            <h1>Foco em estratégia digital</h1>
+                            <p>Especialista em planejamento estratégico e modelo de negócio usando MDP - Marketing digital de posicionamento obtendo mais visitante, leads, oportunidades e cliente para seu negócio, sendo desenvolvido junto ao cliente e o projeto tendo comunicação e marketing que melhor atende as necessidades do negócio.
+                            <br></p>
+                            <h4>Comece agora à fazer um Briefing, que é a primeira etapa para descobrir quais as necessidades do seu negócio.</h4>
+                            <p>Vou estar explicando para você sobre, briefing nada mas e que um trarefa para começar colocar em pauta tudo sobre a sua empresa ou negócio até mesmo o seu produto, e nos mostrar como traçar a melhor linha de execução para elaborar métodos que irão proporcionar resultados positivos no meio digital.</p>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="col block">
+                                    <center>
+                                        <h5>Aumente o tráfego do seu projeto e converta mais!</h5>
+                                    </center>
+                                </div>
+                                <div class="w-100"></div>
+                                <div class="col">
+                                    <i class="fa fa-area-chart fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-cogs fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-globe fa-3x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-trophy fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-wrench fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-thumbs-o-up fa-3x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-file-o fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-handshake-o fa-2x intro"  aria-hidden="true"></i>
+                                    <i class="fa fa-gift fa-3x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-facebook-official fa-2x intro" aria-hidden="true"></i>
+                                    <i class="fa fa-linkedin-square fa-2x intro" aria-hidden="true"></i>
+                                </div>
+                                <div class="w-100"></div>
+                                <div class="col intro">
+                                    <svg viewBox="0 0 373.4 246">
+                                        <polygon class="st0" points="347.3,60.2 29.7,60.2 14.9,10.2 364.9,10.2 "/>
+                                        <polygon class="st1" points="324.4,121.9 52.1,121.9 39.5,71.9 339.5,71.9 "/>
+                                        <polygon class="st2" points="303.8,182 76.9,182 66.4,132 316.4,132 "/>
+                                        <polygon class="st3" points="276.6,241.7 95.1,241.7 86.7,191.7 286.7,191.7 "/>
+                                        <text transform="matrix(1.2374 0 0 1 102.4104 47.4385)" class="st5">Visitantes</text>
+                                        <text transform="matrix(1.2374 0 0 1 144.5128 109.3618)" class="st5">Leads</text>
+                                        <text transform="matrix(1.2374 0 0 1 106.4103 168.3621)" class="st5">Converter</text>
+                                        <text transform="matrix(1.2374 0 0 1 115.4104 228.3623)" class="st5">Clientes</text>
+                                    </svg>
+                                </div>
+                            </div>    
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col">
+                            <button class="btn btn-outline-primary block">Faça já Briefing</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           <?php //require_once "footer.php";?>
+        </div>    
+		<?php //require_once"header.php";?>      
+    <script>
+      var hero = {
+        origin   : "top",
+        distance : "24px",
+        duration : 1500,
+        scale    : 1.05,
+      }
+
+      var intro = {
+        origin   : "bottom",
+        distance : "40px",
+        duration : 900,
+        delay    : 1200,
+        scale    : 1,
+      }
+      var block = {
+        origin   : "top",
+        distance : "40px",
+        duration : 900,
+        delay    : 1200,
+        scale    : 1.05,
+      }
+      sr.reveal(".hero", hero)
+      sr.reveal(".intro", intro)
+      sr.reveal(".block",block)    
+    </script>
+	</body>
+</html>
